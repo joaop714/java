c0b385ad22b3200d7dfecbcbe6ebcf55ca7723f6
JoãoP <152707168+joaop714@users.noreply.github.com>
2024-09-27 14:58:22 -0300

    Lista Alunos
    Atividade Vetor

---
+++ Lista/src/Aluno.java
@@ -0,0 +1,23 @@
+public class Aluno {
+    private String nome;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+
+    @Override
+    public String toString(){
+        return this.nome;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        Aluno oAluno=(Aluno)obj;
+        return this.nome.equals(oAluno.nome);
+    }
+
+}
